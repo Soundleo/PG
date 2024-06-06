2941d8e58c672687ec26feecbb42e3d0b864e597
Soundleo <leorperes@gmail.com>
2024-06-05 21:23:22 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# calcular progressão geométrica
+
+calcular_pg = lambda x: x*2
+
+# lista de numeros
+numeros = [1,2,3,4,5,6,7,8,9]
+
+# mostra os resultados da pg
+
+print(list(map(calcular_pg, numeros)))
